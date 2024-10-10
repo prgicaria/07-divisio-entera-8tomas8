f5781e36bd806fd322d6669fec72ffacdad4fbd2
Tomas <tibanez@instituticaria.cat>
2024-10-10 09:25:11 +0200

    2n enviament

--- divisio.py
+++ divisio.py
@@ -2,6 +2,6 @@ a = int(input("Introdueix el dividend: "))
 b = int(input("Introdueix el divisor: "))
 c = a // b
 d = a % b 
-print(f"Divisió {a}/{b}")
-print(f"Quocient:{c}")
-print(f"Residu:{d}")
+print(f"Divisió: {a}/{b}")
+print(f"Quocient: {c}")
+print(f"Residu: {d}")
